7926f49ca2c6142032e9aafac6c550852b45e4c5
Marek Rucinski <marek.rucinski@esa.int>
2009-03-24 16:29:27 +0100

    Removed logging from const in rate migration scheme

--- src/GOclasses/basic/ConstInRateMigrationScheme.cpp
+++ src/GOclasses/basic/ConstInRateMigrationScheme.cpp
@@ -28,11 +28,11 @@ void ConstInRateMigrationScheme::preEvolutionCallback(island& _island)
 	// Choose a random incoming neighbour and perform the migration.
 	lock_type lock(topology_mutex);
 	
-	std::cout << "preEvolutionCallback( " << _island.id() << " )" << std::endl;
+	//std::cout << "preEvolutionCallback( " << _island.id() << " )" << std::endl;
 	
 	const std::vector<size_t>& neighbours = topology->get_neighbours_in(_island.id());
 	
-	std::cout << "The island has " << neighbours.size() << " neigbours." << std::endl;
+	//std::cout << "The island has " << neighbours.size() << " neigbours." << std::endl;
 	
 	if(neighbours.size() > 0) { //the island must have neighbours
 			
@@ -40,12 +40,12 @@ void ConstInRateMigrationScheme::preEvolutionCallback(island& _island)
 		size_t chosenNeighbourIndex = rng() % neighbours.size();
 		size_t chosenNeighbour = neighbours[chosenNeighbourIndex];
 		
-		std::cout << "Randomly chose neighbour id: " << chosenNeighbour << std::endl;
+		//std::cout << "Randomly chose neighbour id: " << chosenNeighbour << std::endl;
 		
 		//Get neighbour's immigrants
 		std::vector<Individual> immigrants = immigrantsDB[chosenNeighbour];
 		
-		std::cout << "The neighbour provides " << immigrants.size() << " individuals" << std::endl;
+		//std::cout << "The neighbour provides " << immigrants.size() << " individuals" << std::endl;
 		
 		if(immigrants.size() > 0) { //if there's anything to migrate, do it
 			_island.acceptMigratingIndividuals(immigrants);
@@ -58,12 +58,12 @@ void ConstInRateMigrationScheme::postEvolutionCallback(island& _island)
 	//Update _island's database with it's migrating individuals.
 	lock_type lock(topology_mutex);
 	
-	std::cout << "postEvolutionCallback( " << _island.id() << " )" << std::endl;
+	//std::cout << "postEvolutionCallback( " << _island.id() << " )" << std::endl;
 	
 	//Get individuals from the islands
 	std::vector<Individual> immigrants = _island.getMigratingIndividuals();
 	
-	std::cout << "putting " << immigrants.size() << " individuals to the DB" << std::endl;
+	//std::cout << "putting " << immigrants.size() << " individuals to the DB" << std::endl;
 	
 	//Replace the contents of the database for the island
 	immigrantsDB[_island.id()].swap(immigrants);
