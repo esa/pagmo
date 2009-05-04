56f934c7e2eb98704ca882a8c90bb2322761550a
Francesco Biscani <francesco.biscani@esa.int>
2009-05-04 12:32:50 +0200

    Minor formatting fixes.

--- src/keplerian_toolbox/state.h
+++ src/keplerian_toolbox/state.h
@@ -21,13 +21,15 @@ namespace keplerian_toolbox
 		public:
 			typedef T value_type;
 			typedef size_t size_type;
-			state() {
+			state()
+			{
 				for (size_type i = 0; i < Size; ++i) {
 					m_array[i] = value_type(0);
 				}
 			}
 			template <class Vector>
-			state(const Vector &v) {
+			state(const Vector &v)
+			{
 				if (v.size() != Size) {
 					P_EX_THROW(value_error,"invalid vector size while constructing state");
 				}
@@ -35,10 +37,12 @@ namespace keplerian_toolbox
 					m_array[i] = value_type(v[i]);
 				}
 			}
-			size_type size() const {
+			size_type size() const
+			{
 				return Size;
 			}
-			const value_type &operator[](const size_type &n) const {
+			const value_type &operator[](const size_type &n) const
+			{
 				return m_array[n];
 			}
 		protected:
@@ -46,12 +50,14 @@ namespace keplerian_toolbox
 	};
 
 	template <class T, int Size>
-	inline std::ostream &operator<<(std::ostream &o, const state<T,Size> &s) {
+	inline std::ostream &operator<<(std::ostream &o, const state<T,Size> &s)
+	{
 		typedef typename state<T,Size>::size_type size_type;
 		o << std::scientific;
 		o.precision(15);
 		o << "State vector: [";
-		for (size_type i = 0; i < Size; ++i) {
+		for (size_type i = 0; i < Size; ++i)
+		{
 			o << s[i];
 			if (i != Size - 1) {
 				o << ' ';
@@ -71,14 +77,16 @@ namespace keplerian_toolbox
 
 	template <class T>
 	struct cartesian_coordinate_system: public base_coordinate_system<T> {
-		boost::shared_ptr<base_coordinate_system<T> > clone() const {
+		boost::shared_ptr<base_coordinate_system<T> > clone() const
+		{
 			return boost::shared_ptr<base_coordinate_system<T> >(new cartesian_coordinate_system());
 		}
 	};
 
 	template <class T>
 	struct spherical_coordinate_system: public base_coordinate_system<T> {
-		void to_cartesian(boost::array<T,6> &s) const {
+		void to_cartesian(boost::array<T,6> &s) const
+		{
 			// Position.
 			const T &r = s[0], &phi = s[1], &theta = s[2], sin_theta = std::sin(theta),
 				cos_theta = std::cos(theta), sin_phi = std::sin(phi), cos_phi = std::cos(phi);
@@ -94,7 +102,8 @@ namespace keplerian_toolbox
 			s[4] = vy;
 			s[5] = vz;
 		}
-		void from_cartesian(boost::array<T,6> &s) const {
+		void from_cartesian(boost::array<T,6> &s) const
+		{
 			// Position.
 			const T &x = s[0], &y = s[1], &z = s[2];
 			const T r = std::sqrt(x * x + y * y + z * z);
@@ -123,7 +132,8 @@ namespace keplerian_toolbox
 			s[4] = vphi;
 			s[5] = vtheta;
 		}
-		boost::shared_ptr<base_coordinate_system<T> > clone() const {
+		boost::shared_ptr<base_coordinate_system<T> > clone() const
+		{
 			return boost::shared_ptr<base_coordinate_system<T> >(new spherical_coordinate_system());
 		}
 	};
@@ -136,7 +146,8 @@ namespace keplerian_toolbox
 			template <class Vector>
 			pv_state(const Vector &v):ancestor(v),m_cs(new cartesian_coordinate_system<T>()) {}
 			template <class Vector>
-			pv_state(const Vector &pos, const Vector &vel):ancestor(),m_cs(new cartesian_coordinate_system<T>()) {
+			pv_state(const Vector &pos, const Vector &vel):ancestor(),m_cs(new cartesian_coordinate_system<T>())
+			{
 				pv_size_check(pos);
 				pv_size_check(vel);
 				typedef typename ancestor::size_type size_type;
@@ -146,39 +157,46 @@ namespace keplerian_toolbox
 				}
 			}
 			pv_state(const pv_state &pvs):ancestor(pvs),m_cs(pvs.m_cs->clone()) {}
-			pv_state &operator=(const pv_state &pvs) {
+			pv_state &operator=(const pv_state &pvs)
+			{
 				if (this != &pvs) {
 					ancestor::operator=(pvs);
 					m_cs = pvs.m_cs->clone();
 				}
 				return *this;
 			}
-			boost::array<T,3> get_position() const {
+			boost::array<T,3> get_position() const
+			{
 				boost::array<T,3> retval = {{(*this)[0], (*this)[1], (*this)[2]}};
 				return retval;
 			}
-			boost::array<T,3> get_velocity() const {
+			boost::array<T,3> get_velocity() const
+			{
 				boost::array<T,3> retval = {{(*this)[3], (*this)[4], (*this)[5]}};
 				return retval;
 			}
-			boost::shared_ptr<base_coordinate_system<T> > get_coordinate_system() const {
+			boost::shared_ptr<base_coordinate_system<T> > get_coordinate_system() const
+			{
 				return m_cs->clone();
 			}
-			pv_state &set_coordinate_system(const base_coordinate_system<T> &cs) {
+			pv_state &set_coordinate_system(const base_coordinate_system<T> &cs)
+			{
 				m_cs->to_cartesian(this->m_array);
 				m_cs = cs.clone();
 				m_cs->from_cartesian(this->m_array);
 				return *this;
 			}
 			template <class Vector>
-			void set_position(const Vector &p) {
+			void set_position(const Vector &p)
+			{
 				pv_size_check(p);
 				this->m_array[0] = p[0];
 				this->m_array[1] = p[1];
 				this->m_array[2] = p[2];
 			}
 			template <class Vector>
-			void set_velocity(const Vector &v) {
+			void set_velocity(const Vector &v)
+			{
 				pv_size_check(v);
 				this->m_array[3] = v[0];
 				this->m_array[4] = v[1];
@@ -186,7 +204,8 @@ namespace keplerian_toolbox
 			}
 		private:
 			template <class Vector>
-			static void pv_size_check(const Vector &v) {
+			static void pv_size_check(const Vector &v)
+			{
 				if (v.size() != 3) {
 					P_EX_THROW(value_error,"invalid size for position/velocity vector");
 				}
@@ -197,7 +216,8 @@ namespace keplerian_toolbox
 	template <class T>
 	struct base_orbit_propagator {
 		virtual void propagate(std::vector<pv_state<T> > &) const {}
-		virtual bool verify(const std::vector<pv_state<T> > &) const {
+		virtual bool verify(const std::vector<pv_state<T> > &) const
+		{
 			return true;
 		}
 		virtual boost::shared_ptr<base_orbit_propagator> clone() const = 0;
@@ -206,7 +226,8 @@ namespace keplerian_toolbox
 
 	template <class T>
 	struct null_orbit_propagator: public base_orbit_propagator<T> {
-		boost::shared_ptr<base_orbit_propagator<T> > clone() const {
+		boost::shared_ptr<base_orbit_propagator<T> > clone() const
+		{
 			return boost::shared_ptr<base_orbit_propagator<T> >(new null_orbit_propagator());
 		}
 	};
@@ -214,13 +235,15 @@ namespace keplerian_toolbox
 	template <class T>
 	class dynamical_system {
 		public:
-			dynamical_system():m_op(new null_orbit_propagator<T>()),m_states(),m_time(0) {
+			dynamical_system():m_op(new null_orbit_propagator<T>()),m_states(),m_time(0)
+			{
 				if (!m_op->verify(m_states)) {
 					
 				}
 			}
 			dynamical_system(const dynamical_system &b):m_op(b.m_op->clone()),m_states(b.m_states),m_time(b.m_time) {}
-			dynamical_system &operator=(const dynamical_system &b) {
+			dynamical_system &operator=(const dynamical_system &b)
+			{
 				if (this != &b) {
 					m_op = b.m_op->clone();
 					m_states = b.m_states;
@@ -228,7 +251,8 @@ namespace keplerian_toolbox
 				}
 				return *this;
 			}
-			size_t size() const {
+			size_t size() const
+			{
 				return m_states.size();
 			}
 		private:
