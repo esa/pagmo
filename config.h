9fdb40821a9e8e727eecba411044658d7d08d815
Francesco Biscani <francesco.biscani@esa.int>
2009-06-11 14:33:34 +0200

    Copyright notices missed in previous commits.

--- config.h
+++ config.h
@@ -1,5 +1,9 @@
 /*****************************************************************************
- *   Copyright (C) 2008, 2009 Advanced Concepts Team (European Space Agency) *
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
  *   act@esa.int                                                             *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
