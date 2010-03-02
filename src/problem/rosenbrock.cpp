091246bdfeead9ee4034d5d1ee19862e993ea898
Dario Izzo <dario.izzo@googlemail.com>
2010-03-02 23:51:32 +0100

    Rosenbrock problem added. Some test function visual representations added to the docs.

---
+++ src/problem/rosenbrock.cpp
@@ -0,0 +1,64 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+
+#include "../exceptions.h"
+#include "../types.h"
+#include "base.h"
+#include "rosenbrock.h"
+
+namespace pagmo { namespace problem {
+
+/// Constructor from dimension.
+/**
+ * Will construct an n dimensional Rosenbrock problem.
+ *
+ * @param[in] n integer dimension of the problem.
+ *
+ * @see problem::base constructors.
+ */
+rosenbrock::rosenbrock(unsigned int n):base(n)
+{
+	// Set bounds.
+	set_lb(-5.0);
+	set_ub(10);
+}
+
+/// Clone method.
+base_ptr rosenbrock::clone() const
+{
+	return base_ptr(new rosenbrock(*this));
+}
+
+/// Implementation of the objective function.
+void rosenbrock::objfun_impl(fitness_vector &f, const decision_vector &x) const
+{
+	const decision_vector::size_type n = x.size();
+	f[0]=0;
+	for (decision_vector::size_type i=0; i<n-1; ++i){
+		f[0] += 100 * (x[i]*x[i] -x[i+1])*(x[i]*x[i] -x[i+1]) + (x[i]-1)*(x[i]-1);
+	}
+}
+
+}}
---
+++ src/problem/rosenbrock.h
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_PROBLEM_ROSENBROCK_H
+#define PAGMO_PROBLEM_ROSENBROCK_H
+
+#include "../types.h"
+
+namespace pagmo{ namespace problem {
+
+/// The Rosenbrock problem.
+/**
+ * \image html rosenbrock.png "Two-dimensional Rosenbrock function."
+ * \image latex rosenbrock.png "Two-dimensional Rosenbrock function." width=5cm
+ *
+ * The objective function here is the generalised n-dimensional Rosenbrock function:
+ * \f[
+ * 	F\left(x_1,\ldots,x_n\right) =
+ *	\sum_{i=1}^{n-1}\left[ 100\left(x_i^2-x_{i+1}\right)^2+\left(x_i-1\right)^2\right], \quad x_i \in \left[ -5,10 \right].
+ * \f]
+ * The global minimum is in \f$x_i=1\f$, where \f$ F\left( 1,\ldots,1 \right) = 0 \f$.
+ *
+ * @author Dario Izzo (dario.izzo@esa.int)
+ */
+
+class __PAGMO_VISIBLE rosenbrock : public base
+{
+	public:
+		rosenbrock(unsigned int);
+		base_ptr clone() const;
+	protected:
+		void objfun_impl(fitness_vector &, const decision_vector &) const;
+};
+
+}} //namespaces
+
+#endif // ROSENBROCK_H
