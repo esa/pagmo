6eabc9615a4a10cd6e277b35828dab46438bde16
Dario Izzo <dario@act-mainserver.estec.esa.int>
2010-03-08 12:24:31 +0100

    Start to work on the Simple Genetic Algorithm

---
+++ src/algorithm/sga.cpp
@@ -0,0 +1,5 @@
+#include "sga.h"
+
+sga::sga()
+{
+}
---
+++ src/algorithm/sga.h
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_ALGORITHM_SGA_H
+#ifndef PAGMO_ALGORITHM_SGA_H
+
+#include "../config.h"
+#include "base.h"
+#include "../problem/base.h"
+
+
+namespace pagmo { namespace algorithm {
+
+/// The Simple Genetic Algorithm (SGA)
+/**
+ * Genetic algorithms are very popular algorithms used widely by people of very different backgrounds.
+ * As a consequence there are a large number of different implementations and toolboxes that are available
+ * and can be used to construct a genetic algorithm. We decided not to choose one of these and, instead, to
+ * provide only a basic implementation of the algorithm implementing a floating point encoding (not binary)
+ * and some common mutation and crossover strategies, hence the name Simple Genetic Algorithm.
+ *
+ * @author Dario Izzo (dario.izzo@googlemail.com)
+ *
+ */
+
+class __PAGMO_VISIBLE sga: public base
+{
+public:
+	enum selection {BEST20 = 0,ROULETTE = 1};
+	enum mutation {GAUSSIAN = 0, BOUNDED = 1};
+	sga(int gen, const double &cr, const double &mut_rate, int elitism = 1, mutation mut  = BOUNDED, selection sel = ROULETTE, double mut_range = 1);
+	base_ptr clone() const;
+	void evolve(population &) const;
+protected:
+	std::string human_readable_extra() const;
+private:
+	//Number of generations
+	const int m_gen;
+	//Crossover rate
+	const double& m_cr;
+	//Mutation rate
+	const double& m_mut_rate;
+	//Elitism (number of generations after which to reinsert the best)
+	const int m_elitism;
+	//Mutation type
+	const mutation m_mut;
+	//Selection_type
+	const selection m_sel;
+	//Mutation range
+	const double m_mut_range;
+
+};
+
+}} //namespaces
+
+#endif // PAGMO_ALGORITHM_SGA_H
