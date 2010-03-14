47c15c1b4667be5166d2664f92a4c09878c54068
Francesco Biscani <francesco.biscani@esa.int>
2010-03-14 17:50:57 +0100

    Added files for string matching problems.

---
+++ src/problem/string_match.cpp
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#include <boost/numeric/conversion/cast.hpp>
+#include <cmath>
+#include <string>
+
+#include "../types.h"
+#include "base.h"
+#include "string_match.h"
+
+namespace pagmo { namespace problem {
+
+string_match::string_match(const std::string &str):base(boost::numeric_cast<int>(str.size()),boost::numeric_cast<int>(str.size()),1,0,0),m_str(str)
+{
+	set_bounds(0,255);
+}
+
+string_match::string_match(const char *str):base(boost::numeric_cast<int>(std::string(str).size()),boost::numeric_cast<int>(std::string(str).size()),1,0,0),m_str(str)
+{
+	set_bounds(0,255);
+}
+
+base_ptr string_match::clone() const
+{
+	return base_ptr(new string_match(*this));
+}
+
+void string_match::objfun_impl(fitness_vector &f, const decision_vector &x) const
+{
+	double retval = 0;
+	for (std::string::size_type i = 0; i < m_str.size(); ++i) {
+		retval += std::abs(static_cast<char>(x[boost::numeric_cast<size_type>(i)]) - m_str[i]);
+	}
+	f[0] = retval;
+}
+
+} }
---
+++ src/problem/string_match.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_PROBLEM_STRING_MATCH_H
+#define PAGMO_PROBLEM_STRING_MATCH_H
+
+#include <string>
+
+#include "../config.h"
+#include "../types.h"
+#include "base.h"
+
+namespace pagmo { namespace problem {
+
+class __PAGMO_VISIBLE string_match: public base
+{
+	public:
+		string_match(const std::string &);
+		string_match(const char *);
+		base_ptr clone() const;
+	protected:
+		void objfun_impl(fitness_vector &, const decision_vector &) const;
+		//std::string human_readable_extra() const;
+	private:
+		const std::string m_str;
+};
+
+} }
+
+#endif
