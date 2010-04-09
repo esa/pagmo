02d0ddea0bb413b9589fba9736eb231c3b1f5f82
Francesco Biscani <francesco.biscani@esa.int>
2010-04-09 11:22:56 +0200

    Added file to test for GSL version.

---
+++ cmake_modules/gsl_version.cpp
@@ -0,0 +1,23 @@
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/lexical_cast.hpp>
+#include <gsl/gsl_version.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::vector<std::string> v;
+	boost::split(v,GSL_VERSION,boost::algorithm::is_any_of("."));
+	std::cout << "\n\tDetected GSL version: " << GSL_VERSION;
+	if (v.size() < 2) {
+		return -1;
+	}
+	if (boost::lexical_cast<int>(v[0]) < 1) {
+		return -1;
+	} else if (boost::lexical_cast<int>(v[0]) == 1 && boost::lexical_cast<int>(v[1]) < 13) {
+		return -1;
+	}
+	return 0;
+}
