d0eebd039905ecb765949ae82195697dade12a09
Francesco Biscani <francesco.biscani@esa.int>
2010-04-13 15:04:19 +0200

    Re-enable preliminary Python test suite.

--- PyGMO/__init__.py
+++ PyGMO/__init__.py
@@ -24,3 +24,23 @@
 from core import *
 import algorithm, migration, problem, topology
 from copy import copy
+
+def run_test():
+	from PyGMO import problem, algorithm, island
+	from numpy import mean, std
+	prob_list = [problem.schwefel(10),problem.rastrigin(10),problem.rosenbrock(10)]
+	algo_list = [algorithm.de(500,0.8,0.8,2),algorithm.sa_corana(10000,1,0.1),
+		algorithm.ihs(10000)]
+	for algo in algo_list:
+		print('Testing algorithm: ' + str(algo))
+		for prob in prob_list:
+			print('\tTesting problem: ' + str(type(prob)) + ', Dimension: ' + str(prob.dimension))
+			best = []
+			for i in range(0,100):
+				isl = island(prob,algo,20)
+				isl.evolve(1)
+				isl.join()
+				best.append(isl.population.champion.f)
+			print('\t\tBest:\t' + str(min(best)))
+			print('\t\tMean:\t' + str(mean(best)))
+			print('\t\tStd:\t' + str(std(best)))
