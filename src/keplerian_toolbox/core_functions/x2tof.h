594c222ab4d4383dd995b2c5ba93965a0317e905
Dario Izzo <dario.izzo@googlemail.com>
2010-04-26 00:56:59 +0200

    x2tof.h added (Battin variable time of flight finder) main restored

--- src/keplerian_toolbox/core_functions/x2tof.h
+++ src/keplerian_toolbox/core_functions/x2tof.h
@@ -22,27 +22,60 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
  *****************************************************************************/
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <boost/algorithm/string.hpp>
-#include <boost/lexical_cast.hpp>
-#include"src/pagmo.h"
+#ifndef X2TOF_H
+#define X2TOF_H
 
-using namespace pagmo;
-using namespace kep_toolbox;
+#include<cmath>
 
-int main()
+#include"boost/math/special_functions/acosh.hpp"
+#include"boost/math/special_functions/asinh.hpp"
+
+namespace kep_toolbox {
+/// Convert the Battin's variable x to time of flight in a non dimensional Lambert's problem
+/**
+ * A Lambert problem is defined by \f$ r_1,r_2,t\f$ and \f$ \mu\f$. Assuming as length units \f$ |r_1|\f$ and as velocity
+ * units \f$ \sqrt(\mu/|r_1|)\f$ (i.e. \f$ \mu = 1\f$) the Lambert's problem is only defined by \f$ r_2,t\f$. Employing these units
+ * this function returns the time-of flight from the Battin's variable \f$ x\f$.
+ *
+ * \param[in] x Battin's variable
+ * \param[in] s half perimeter of the triangle defined by r1,r2
+ * \param[in] c chord defined by the r1,r2 triangle
+ * \param[in] lw switch between long and short way solutions
+ *
+ * \see Battin "Introduction to the Mahematics and Methods of Astrodynamic"
+ *
+ * @author Dario Izzo (dario.izzo _AT_ googlemail.com)
+ */
+inline sdouble x2tof(const double &x,const double &s,const double &c,const int &lw)
 {
+	double am,a,alfa,beta;
 
-	algorithm::sa_corana algo(10000,10,0.001);
-	problem::levy5 prob(10);
-	archipelago a(prob,algo,2,20,::pagmo::topology::ring());
-	std::cout << algo << std::endl;
-	std::cout << "To solve:" << '\n' << prob << std::endl;
-	for (int i = 0; i<200; ++i) {
-		a.evolve();
-		std::cout << a.get_island(0).get_population().champion().f << std::endl;
+	am = s/2;
+	a = am/(1-x*x);
+	if (x < 1)	//ellpise
+	{
+		beta = 2 * asin (sqrt((s - c)/(2*a)));
+		if (lw) beta = -beta;
+		alfa = 2 * acos(x);
 	}
-	return 0;
+	else
+	{
+		alfa = 2 * boost::math::acosh(x);
+		beta = 2 * boost::math::asinh(sqrt ((s - c)/(-2 * a)));
+		if (lw) beta = -beta;
+	}
+
+	if (a > 0)
+	{
+		return (a * sqrt (a)* ( (alfa - sin(alfa)) - (beta - sin(beta)) ));
+	}
+	else
+	{
+		return (-a * sqrt(-a)*( (sinh(alfa) - alfa) - ( sinh(beta) - beta)) );
+	}
+
 }
+
+} //namespace
+
+#endif // X2TOF_H
