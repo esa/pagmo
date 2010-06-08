676386792ff50f6f963ed865267a2119c676ee39
Francesco Biscani <francesco.biscani@esa.int>
2010-06-08 21:06:32 +0200

    Removed stale files.

--- build/src/CMakeFiles/pagmo_static.dir/cmake_clean_target.cmake
+++
@@ -1,4 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libpagmo_static.a"
-  "libpagmo_static.so"
-)
--- build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dante/pagmo/src
-# Build directory: /home/dante/pagmo/build/src
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
