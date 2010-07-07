59d3567ef59df76794ca9a69ea4519783d6a03c0
Francesco Biscani <francesco.biscani@esa.int>
2010-07-07 14:11:36 +0200

    Removed stale files.

--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dante/pagmo/tests
-# Build directory: /home/dante/pagmo/build/tests
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
-ADD_TEST(test_001 "test_001")
-ADD_TEST(test_serialization "test_serialization")
