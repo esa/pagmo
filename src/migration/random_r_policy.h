281f5f5357fec883dc1bc4d097f002fcac75d00a
Dario <dario@dario-1015PEM.(none)>
2011-09-06 20:18:24 +0200

    Fixed bug in island constructors: kwargs are now passed correctly. Changed all default values of migration.rate_type to absolute

--- src/migration/random_r_policy.h
+++ src/migration/random_r_policy.h
@@ -48,7 +48,7 @@ namespace pagmo { namespace migration {
 class __PAGMO_VISIBLE random_r_policy: public base_r_policy
 {
 	public:
-		random_r_policy(const double &rate = 1, rate_type type = fractional);
+		random_r_policy(const double &rate = 1, rate_type type = absolute);
 		base_r_policy_ptr clone() const;
 		std::vector<std::pair<population::size_type,std::vector<population::individual_type>::size_type> >
 			select(const std::vector<population::individual_type> &, const population &) const;
