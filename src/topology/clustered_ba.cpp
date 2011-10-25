7848a4844da272b30c3dce724b0ee7b8c76e3c04
Luke <lumoconnor@gmail.com>
2011-10-25 11:22:38 +0200

    First commit to add the Clustered Barabasi-Albert Topology

---
+++ src/topology/clustered_ba.cpp
@@ -0,0 +1,164 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <cstddef>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <utility>
+
+
+#include "../exceptions.h"
+#include "../rng.h"
+#include "clustered_ba.h"
+#include "base.h"
+
+namespace pagmo { namespace topology {
+
+/// Constructor from kernel size and number of edges.
+/**
+ * Build a BA network in which the initial kernel has size m0 and the elements being inserted after the construction of the kernel
+ * is completed are connected randomly to m nodes. Will fail if m0 < 2, if m < 1 or if m > m0.
+ *
+ * @param[in] m0 size of the kernel
+ * @param[in] m number of random connections to be established when a new node is added.
+ */
+clustered_ba::clustered_ba(int m0, int m):
+	m_m0(boost::numeric_cast<std::size_t>(m0)),m_m(boost::numeric_cast<std::size_t>(m)),
+	m_drng(rng_generator::get<rng_double>()),m_urng(rng_generator::get<rng_uint32>())
+{
+	if (m0 < 2 || m < 1 || m > m0) {
+		pagmo_throw(value_error,"the value of m and m0 must be at least 1 and 2, and m must not be greater than m0");
+	}
+}
+
+base_ptr clustered_ba::clone() const
+{
+        return base_ptr(new clustered_ba(*this));
+}
+
+void clustered_ba::connect(const vertices_size_type &idx)
+{
+	pagmo_assert(get_number_of_vertices() > 0);
+	const vertices_size_type prev_size = get_number_of_vertices() - 1;
+	if (prev_size < m_m0) {
+		// If we had not built the initial m0 nodes, do it.
+		// We want to connect the newcomer island with high probability, and make sure that
+		// at least one connection exists (otherwise the island stays isolated).
+		// NOTE: is it worth to make it a user-tunable parameter?
+		const double prob = 0.8;
+		// Flag indicating if at least 1 connection was added.
+		bool connection_added = false;
+		// Main loop.
+		for (std::pair<v_iterator,v_iterator> vertices = get_vertices(); vertices.first != vertices.second; ++vertices.first) {
+			// Do not consider the new vertex itself.
+			if (*vertices.first != idx) {
+				if (m_drng() < prob) {
+					connection_added = true;
+					// Add the connections
+					add_edge(*vertices.first,idx);
+					add_edge(idx,*vertices.first);
+				}
+			}
+		}
+		// If no connections were established and this is not the first island being inserted,
+		// establish at least one connection with a random island other than n.
+		if ((!connection_added) && (prev_size != 0)) {
+			// Get a random vertex index between 0 and n_vertices - 1. Keep on repeating the procedure if by
+			// chance we end up on idx again.
+			boost::uniform_int<vertices_size_type> uni_int(0,get_number_of_vertices() - 1);
+			vertices_size_type rnd;
+			do {
+				rnd = uni_int(m_urng);
+			} while (rnd == idx);
+			// Add connections to the random vertex.
+			add_edge(rnd,idx);
+			add_edge(idx,rnd);
+		}
+	} else {
+		// Let's find the current total number of edges.
+		const edges_size_type n_edges = get_number_of_edges();
+		pagmo_assert(n_edges > 0);
+		// Now we need to add m edges, choosing the nodes with a probability
+		// proportional to their number of connections. We keep track of the
+		// connection established in order to avoid connecting twice to the same
+		// node.
+		boost::uniform_int<edges_size_type> uni_int(0,n_edges - 1);
+		std::size_t i = 0;
+		std::pair<v_iterator,v_iterator> vertices;
+		std::pair<a_iterator,a_iterator> adj_vertices;
+		while (i < m_m) {
+			// Here we choose a random number between 0 and n_edges - 1.
+			const edges_size_type rn = uni_int(m_urng);
+			edges_size_type n = 0;
+			// Iterate over all vertices and accumulate the number of edges for each of them. Stop when the accumulated number of edges is greater
+			// than rn. This is equivalent to giving a chance of connection to vertex v directly proportional to the number of edges departing from v.
+			// You can think of this process as selecting a random edge among all the existing edges and connecting to the vertex from which the
+			// selected edge departs.
+			vertices = get_vertices();
+			for (; vertices.first != vertices.second; ++vertices.first) {
+				// Do not consider it_n.
+				if (*vertices.first != idx) {
+					adj_vertices = get_adjacent_vertices(*vertices.first);
+					n += boost::numeric_cast<edges_size_type>(std::distance(adj_vertices.first,adj_vertices.second));
+					if (n > rn) {
+						break;
+					}
+				}
+			}
+			pagmo_assert(vertices.first != vertices.second);
+			// If the candidate was not already connected, then add it.
+			if (!are_adjacent(idx,*vertices.first)) {
+				add_edge(*vertices.first,idx);
+				add_edge(idx,*vertices.first);
+				++i;
+			}
+		}
+	}
+}
+
+/// Topology-specific human readable info.
+/**
+ * Will return a formatted string containing the size of the kernel and the number of connections for newly-inserted nodes.
+ *
+ * @return string containing the parameters of the BA model.
+ */
+std::string clustered_ba::human_readable_extra() const
+{
+	std::ostringstream oss;
+	oss << "\tm0 = " << m_m0 << '\n';
+	oss << "\tm = " << m_m << '\n';
+	return oss.str();
+}
+
+std::string clustered_ba::get_name() const
+{
+	return "Barabasi-Albert";
+}
+
+}} //namespaces
+
+BOOST_CLASS_EXPORT_IMPLEMENT(pagmo::topology::clustered_ba);
---
+++ src/topology/clustered_ba.h
@@ -0,0 +1,88 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_TOPOLOGY_CLUSTERED_BA_H
+#define PAGMO_TOPOLOGY_CLUSTERED_BA_H
+
+#include <cstddef>
+#include <string>
+
+#include "../config.h"
+#include "../rng.h"
+#include "../serialization.h"
+#include "base.h"
+
+namespace pagmo { namespace topology {
+
+/// Barabási-Albert graph topology.
+/**
+ * \image html ba.png "Barabási-Albert network with m0 = 3, m = 2 and 100 vertices."
+ * \image latex ba_large.png "Barabási-Albert network with m0 = 3, m = 2 and 100 vertices." width=7cm
+ *
+ * Topology based on the Barabási-Albert (BA) model for the generation of random undirected scale-free networks. The construction of this topology consists internally of
+ * two phases:
+ * - the first m0 elements added to the network constitute a kernel of nodes connected to each other with high probability;
+ * - after the kernel is built, the next elements added to the network are connected randomly to m of the existing nodes; the probability
+ *   of connection is biased linearly towards the most connected nodes.
+ *
+ * @author Francesco Biscani (bluescarni@gmail.com)
+ * @author Marek Ruciński (marek.rucinski@gmail.com)
+ *
+ * @see http://en.wikipedia.org/wiki/BA_model
+ */
+class __PAGMO_VISIBLE clustered_ba : public base
+{
+	public:
+                clustered_ba(int m0 = 3, int m = 2);
+		base_ptr clone() const;
+		std::string get_name() const;
+	protected:
+		void connect(const vertices_size_type &);
+		std::string human_readable_extra() const;
+	private:
+		friend class boost::serialization::access;
+		template <class Archive>
+		void serialize(Archive &ar, const unsigned int)
+		{
+			ar & boost::serialization::base_object<base>(*this);
+			ar & const_cast<std::size_t &>(m_m0);
+			ar & const_cast<std::size_t &>(m_m);
+			ar & m_drng;
+			ar & m_urng;
+		}  
+		// Size of the kernel - the starting number of nodes.
+		const std::size_t	m_m0;
+		// Number of edges per newly-inserted node.
+		const std::size_t	m_m;
+		// Double random number generator
+		rng_double		m_drng;
+		// Integer random number generator.
+		rng_uint32		m_urng;
+};
+
+}}
+
+BOOST_CLASS_EXPORT_KEY(pagmo::topology::clustered_ba);
+
+#endif
