1bbd5876ba52e1851860a0d1f47935b0515caca7
Dario Izzo <dario.izzo@googlemail.com>
2011-11-02 13:36:48 +0100

    CMAES - Finished (python version)

--- PyGMO/algorithm/_cmaes.py
+++ PyGMO/algorithm/_cmaes.py
@@ -4,15 +4,17 @@ class py_cmaes(base):
 	"""
 	Covariance Matrix Adaptation Evolutionary Strategy (Python)
 	"""
-	def __init__(self, gen = 500, elite = 0.5, cc = -1, cs = -1, c1 = -1, cmu = -1, sigma0=0.5, screen_output = False):
+	def __init__(self, gen = 500, elite = 0.5, cc = -1, cs = -1, c1 = -1, cmu = -1, sigma0=0.5, ftol = 1e-6, xtol = 1e-6, fabs = -1.0**300, screen_output = False):
 		"""
 		Covariance Matrix Adaptation Evolutionary Strategy (Python)
 
 		USAGE: algorithm.py_cmaes(gen = 500, elite = 0.5, cc = -1, cs = -1, c1 = -1, cmu = -1, screen_output = False)
 
 		NOTE: In our variant of the algorithm, particle memory is used to extract the elite and reinsertion
-		is made aggressively ..... geting rid ofthe worst guy). Also the bounds of the problem
-		are enforced as to allow PaGMO machinery to work
+		is made aggressively ..... getting rid of the worst guy). Also, the bounds of the problem
+		are enforced, as to allow PaGMO machinery to work. Fine control on each iteration can be achieved
+		by calling the algo with gen=1 (algo state is stored, cmaes will continue at next call ... without
+		initializing again all its state!!)
 
 		* gen: number of generations
 		* elite: fraction of the population considered as elite (in (0,1])
@@ -21,6 +23,9 @@ class py_cmaes(base):
 		* c1: learning rate for rank-1 update (in [0,1]) if -1 automatic values are set
 		* cmu: learning rate for rank-mu update (in [0,1]) if -1 automatic values are set
 		* sigma0: starting step (std)
+		* xtol: stopping criteria on the x tolerance
+		* ftol: stopping criteria on the f tolerance
+		* fabs: stopping criteria on the f value reached
 		* screen_output: activates screen_output (output at each generation)
 		"""
 		try:
@@ -41,14 +46,34 @@ class py_cmaes(base):
 			raise ValueError("cmu needs to be in [0,1]")
 
 		base.__init__(self)
+
+		#Data members
 		self.__cc = cc
 		self.__cs = cs
 		self.__c1 = c1
 		self.__cmu = cmu
 		self.__gen = gen
 		self.__elite = elite
+		self.__xtol = xtol
+		self.__ftol = ftol
+		self.__fabs = fabs
 		self.__sigma0 = sigma0
 		self.screen_output = screen_output
+
+		#Algorithm memory
+		self.__mean = 0
+		self.__variation = 0
+		self.__newpop = np.matrix([[1]])
+		self.__chiN = 0
+		self.__B = 0
+		self.__D = 0
+		self.__C = 0
+		self.__invsqrtC = 0
+		self.__pc = 0
+		self.__ps = 0
+		self.__counteval = 0
+		self.__eigeneval = 0
+
 		np.random.seed()
 
 	def evolve(self,pop):
@@ -82,7 +107,7 @@ class py_cmaes(base):
 		weights = [w / sum(weights) for w in weights]			# weights for weighted recombination
 		mueff = sum(weights)**2 / sum(w**2 for w in weights)		# variance-effectiveness of sum w_i x_i
 
-		# Setting coefficients for Adaptation
+		# Setting coefficients for Adaptation automatically or to user defined data
 		if self.__cc == -1:
 			cc = (4 + mueff/N) / (N+4 + 2*mueff/N);			# t-const for cumulation for C
 		if self.__cs == -1:
@@ -93,20 +118,34 @@ class py_cmaes(base):
 			cmu = 2 * (mueff-2+1/mueff) / ((N+2)**2+mueff);		# and for rank-mu update
 
 		damps = 1 + 2*max(0, sqrt((mueff-1)/(N+1))-1) + cs;		#damping for sigma
-
-		# Initializing and allocationg
-		mean = matrix(pop.champion.x).T
-		variation = array([[0.0]*N]*lam)
-		newpop = matrix([[0.0]*lam]*N)
 		chiN = N**0.5*(1-1/(4*N)+1/(21*N**2))				#expectation of ||N(0,I)|| == norm(randn(N,1))
-		B = matrix(eye(N,N));						#B defines the coordinate system
-		D = ones(N);							#diagonal D defines the scaling
-		C = matrix(eye(N,N));						#covariance matrix C
-		invsqrtC = matrix(eye(N,N));					#inverse of sqrt(C)
-		pc =matrix([[0]]*N)
-		ps = matrix([[0]]*N)
-		counteval = 0
-		eigeneval = 0
+
+		# Initializing and allocating
+		if not self.__newpop.shape==(N,lam):
+			mean = matrix(pop.champion.x).T
+			variation = array([[0.0]*N]*lam)
+			newpop = matrix([[0.0]*lam]*N)
+			B = matrix(eye(N,N));				#B defines the coordinate system
+			D = ones(N);					#diagonal D defines the scaling
+			C = matrix(eye(N,N));				#covariance matrix C
+			invsqrtC = matrix(eye(N,N));			#inverse of sqrt(C)
+			pc =matrix([[0]]*N)
+			ps = matrix([[0]]*N)
+			counteval = 0
+			eigeneval = 0
+		else:
+			mean = self.__mean
+			variation = self.__variation
+			newpop = self.__newpop
+			B = self.__B
+			D = self.__D
+			C = self.__C
+			invsqrtC = self.__invsqrtC
+			pc = self.__pc
+			ps = self.__ps
+			counteval = self.__counteval
+			eigeneval = self.__eigeneval
+
 		sigma=self.__sigma0
 
 		# Let's start the algorithm
@@ -138,13 +177,13 @@ class py_cmaes(base):
 			#elite = [matrix(ind.cur_x).T for ind in a]
 			#elite = elite[:mu]
 
-			#3 - We compute the new elite mean storing the ol one
+			#3 - Compute the new elite mean storing the old one
 			meanold=mean
 			mean = elite[0]*weights[0]
 			for i in range(1,mu):
 				mean += elite[i]*weights[i]
 
-			#4 Update evolution paths
+			#4 - Update evolution paths
 			ps = (1 - cs)*ps + sqrt(cs*(2-cs)*mueff)* invsqrtC * (mean-meanold) / sigma
 			hsig = ((ps.T*ps)[0,0] / (1-(1-cs)**(2*counteval/lam)) / N) < (2 + 4/(N+1));
 			hsig = int(hsig)
@@ -162,26 +201,62 @@ class py_cmaes(base):
 			sigma *= exp( (cs/damps)*(norm(ps)/chiN - 1));
 
 			#7 - Perform eigen-decomposition of C
-			#if ( (counteval - eigeneval) > (lam/(c1+cmu)/N/10) ):		#achieve O(N^2)
-			eigeneval = counteval;
-			C = (C+C.T)/2					#enforce symmetry
-			D,B = eig(C);					#eigen decomposition, B==normalized eigenvectors
-			D = [s**0.5 for s in D]				#D contains standard deviations now
+			if ( (counteval - eigeneval) > (lam/(c1+cmu)/N/10) ):		#achieve O(N^2)
+				eigeneval = counteval;
+				C = (C+C.T)/2					#enforce symmetry
+				D,B = eig(C);					#eigen decomposition, B==normalized eigenvectors
+				D = [s**0.5 for s in D]				#D contains standard deviations now
 			#if not (0 in D):				#Avoids numerical nans skipping evaluation of invsqrtC
-			invsqrtC = B*diag([1/d for d in D])*B.T
+				invsqrtC = B*diag([1/d for d in D])*B.T
 
-
-			#8 - We print to screen if necessary
+			#8 - Print to screen if necessary
 			if self.screen_output:
 				if not(gen%20):
 					print "\nGen.\tChampion\tHighest\t\tLowest\t\tVariation\t\tstep"
 				print "%d\t%e\t%e\t%e\t%e\t%e" % (gen,pop.champion.f[0],
 				    max([ind.cur_f[0] for ind in pop]),min([ind.cur_f[0] for ind in pop]),
 				    norm(d_mu), sigma)
+
+			#9 - Check the exit conditions (every 40 generations)
+			if not(gen%40):
+				if (norm(d_mu) < self.__xtol):
+					if self.screen_output:
+						print("Exit condition -- xtol < ") + str(self.__xtol)
+					return pop
+
+				if (pop.champion.f[0] < self.__fabs):
+					if self.screen_output:
+						print("Exit condition -- fabs < ") + str(self.__fabs)
+					return pop
+
+				tmp = abs(pop[pop.get_worst_idx()].best_f[0] - pop[pop.get_best_idx()].best_f[0])
+
+				if (tmp < self.__ftol):
+					if self.screen_output:
+						print("Exit condition -- ftol < ") + str(self.__ftol)
+					return pop
+
+		#Update algorithm memory
+		self.__mean = mean
+		self.__variation = variation
+		self.__newpop = newpop
+		self.__chiN = chiN
+		self.__B = B
+		self.__D = D
+		self.__C = C
+		self.__invsqrtC = invsqrtC
+		self.__pc = pc
+		self.__ps = ps
+		self.__counteval = counteval
+		self.__eigeneval = eigeneval
+		self.__sigma0 = sigma
+
+		if self.screen_output:
+			print("Exit condition -- iteration > ") + str(self.__gen)
 		return pop
 
 
 	def get_name(self):
 		return "CMAES (Python)"
 	def human_readable_extra(self):
-		return "gen=" + str(self.__gen) + " elite fraction=" + str(self.__elite) + " cc=" + str(self.__cc) + " cs=" + str(self.__cs) + " c1=" + str(self.__c1) + " cmu=" + str(self.__cmu) + " sigma0=" + str(self.__sigma0)
+		return "gen=" + str(self.__gen) + " elite fraction=" + str(self.__elite) + " cc=" + str(self.__cc) + " cs=" + str(self.__cs) + " c1=" + str(self.__c1) + " cmu=" + str(self.__cmu) + " sigma0=" + str(self.__sigma0) + " xtol=" + str(self.__xtol) + " ftol=" + str(self.__ftol) + " fabs=" + str(self.__fabs)
