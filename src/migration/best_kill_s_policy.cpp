2351642ad5ae5c1da292a46adf64da3de77a39a4
Dario Izzo <dario.izzo@googlemail.com>
2011-11-28 17:12:23 +0100

    A new selection policy that alows to 'kill' individuals once selected

---
+++ src/migration/best_kill_s_policy.cpp
@@ -0,0 +1,69 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2009 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#include <algorithm>
+#include <vector>
+
+#include "../population.h"
+#include "base.h"
+#include "base_s_policy.h"
+#include "best_kill_s_policy.h"
+
+namespace pagmo { namespace migration {
+
+/// Constructor from migration rate and type.
+/**
+ * @param[in] rate migration rate.
+ * @param[in] type migration rate type.
+ *
+ * @see base_s_policy::base_s_policy.
+ */
+best_kill_s_policy::best_kill_s_policy(const double &rate, rate_type type):base_s_policy(rate,type) {}
+
+base_s_policy_ptr best_kill_s_policy::clone() const
+{
+	return base_s_policy_ptr(new best_kill_s_policy(*this));
+}
+
+std::vector<population::individual_type> best_kill_s_policy::select(population &pop) const
+{
+	const population::size_type migration_rate = get_n_individuals(pop);
+	// Create a temporary array of individuals.
+	std::vector<population::individual_type> result(migration_rate);
+	// Get the idx of the best ones (dominance criteria here)
+	std::vector<population::size_type> best_idx = pop.get_best_idx(migration_rate);
+	// Copy them into results
+	for (population::size_type i=0 ; i<migration_rate; ++i) {
+		result[i] = pop.get_individual(best_idx[i]);
+	}
+	// Kill them from the population (note: the champion will still carry information on the guy ...)
+	for (population::size_type i=0 ; i<migration_rate; ++i) {
+		pop.reinit(best_idx[i]);
+	}
+	return result;
+}
+
+}}
+
+BOOST_CLASS_EXPORT_IMPLEMENT(pagmo::migration::best_kill_s_policy);
