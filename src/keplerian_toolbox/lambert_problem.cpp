32956f3adad036a0206cd0a220cf5ffea3073465
Annalisa Riccardi <nina1983@gmail.com>
2013-05-28 16:50:57 +0200

    compilation of GTOC database with MSVC 11

--- src/keplerian_toolbox/lambert_problem.cpp
+++ src/keplerian_toolbox/lambert_problem.cpp
@@ -32,6 +32,9 @@ namespace kep_toolbox {
 const array3D lambert_problem::default_r1 = {{1,0,0}};
 const array3D lambert_problem::default_r2 = {{0,1,0}};
 
+const array3D& lambert_problem::get_default_r1(){return default_r1;}
+const array3D& lambert_problem::get_default_r2(){return default_r2;}	
+
 /// Constructor
 /** It constructs and solves a Lambert problem.
  *
