98308d9cc730ba44c2245715a840c8ded63810f7
Yung Siang Liau <liauys@gmail.com>
2013-08-05 01:03:02 +0800

    Seeding control in Python example

--- PyGMO/examples/benchmark_racing.py
+++ PyGMO/examples/benchmark_racing.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 prob_orig = problem.ackley(10)
 prob = problem.noisy(prob_orig, param_second = 0.5)
 fevals_max = 200000
+#fevals_max = 5000
 pop_size = 20
-pop_seed = 1234
 
 num_trials = 30
 
@@ -20,28 +20,38 @@ def get_pso_gen_racing():
     algo = algorithm.pso_gen(100000,0.7298,2.05,2.05,0.05,5,2,4,True,fevals_max)
     return algo
 
-def run_algo():
+def run_algo(algo, seed):
 
-    pop = population(prob, pop_size, pop_seed)
+    algo.reset_rngs(seed)
+
+    pop = population(prob, pop_size, seed)
 
     pop = algo.evolve(pop)
 
     champion_fitness = prob_orig.objfun(pop.champion.x)
-
-    print 'Final champion =', champion_fitness
+    #print 'Final champion =', champion_fitness
 
     return champion_fitness
 
 if __name__ == '__main__':
     fitnesses = []
+    fitnesses_racing = []
 
     algo = get_pso_gen()
-    #algo = get_pso_gen_racing()
+    algo_with_racing = get_pso_gen_racing()
 
+    seed = 1234
     for i in range(num_trials):
         print '::: Trial #%d :::' % i
-        fitnesses.append(run_algo()[0])
-        pop_seed += 177
+        seed += 177
+        fitnesses.append(run_algo(algo, seed)[0])
+        fitnesses_racing.append(run_algo(algo_with_racing, seed)[0])
 
     avg_fitness = np.mean(fitnesses)
-    print 'Final averaged over %d trials =' % num_trials, avg_fitness
+    avg_fitness_racing = np.mean(fitnesses_racing)
+
+    print '----------------------------------------------'
+    print 'Final averaged actual fitness over %d trials:' % num_trials
+    print 'pso_gen without racing: %f' % avg_fitness
+    print 'pso_gen with racing: %f' % avg_fitness_racing
+    print '----------------------------------------------'
