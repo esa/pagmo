c986d4d63eaec39cecd94911deeb87b0ad784b5c
Dario Izzo <dario.izzo@gmail.com>
2014-11-21 17:05:53 +0100

    first commit of tsp_ads

---
+++ src/problem/tsp_ads.cpp
@@ -0,0 +1,458 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2014 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#include "tsp_ads.h"
+#include "../exceptions.h"
+#include "../population.h"
+#include "../keplerian_toolbox/astro_constants.h"
+
+
+namespace pagmo { namespace problem {
+
+    /// Constructor
+    /**
+     * Constructs a TSP - Asteroids-Debris Selection Problem from 
+     * path length and the chosen encoding
+     *
+     * @param[in] planets         an std::vector of kep_toolbox::planet_ptr containing the orbiting objects one wants to visit
+     * @param[in] values          a std::vector representing the value (score) of a visit (is the same order as planets)
+     * @param[in] max_DV          the maximum DV available on board the spacecraft (corresponding to max_path_length parameter in the problem::tsp_cs problem)
+     * @param[in] times           an std::vector of doubles containing [t0,T1,T2,...] the starting mission epoch and the time of flights between orbiting objects
+     * @param[in] waiting_time    waiting time at one orbiting object before the next transfer starts (i.e. t_0+T_1+waiting_time+.....) 
+     * @param[in] encoding        a pagmo::problem::tsp::encoding representing the chosen encoding
+     */
+    tsp_ads::tsp_ads (            
+        const std::vector<kep_toolbox::planet_ptr>& planets, 
+        const std::vector<double>& values,
+        const double max_path_lenght, 
+        const std::vector<double>&  times, 
+        const double waiting_time, 
+        const base_tsp::encoding_type & encoding
+        ) : base_tsp(
+                planets.size(), 
+                compute_dimensions(planets.size(), encoding)[0],
+                compute_dimensions(planets.size(), encoding)[1],
+                encoding
+        ),  m_planets(planets), m_values(values), m_max_DV(max_DV), m_times(times), m_waiting_time(waiting_time)
+    {
+        if (planets.size() != values.size()){
+            pagmo_throw(value_error, "Planet list must have the same size as values list");
+        }
+        if (planets.size() != times.size()){
+            pagmo_throw(value_error, "Planet list must have the same size as times list");
+        }
+        if (planets.size() < 3 ){
+            pagmo_throw(value_error, "Planet list must contain at least 3 elements");
+        }
+
+        //TODO: check that all planets have the same mu
+
+        m_min_value = *std::min(m_values.begin(), m_values.end());
+    }
+
+    /// Clone method.
+    base_ptr tsp_ads::clone() const
+    {
+        return base_ptr(new tsp_ads(*this));
+    }
+
+    boost::array<int, 2> tsp_cs::compute_dimensions(decision_vector::size_type n_cities, base_tsp::encoding_type encoding)
+    {
+        boost::array<int,2> retval;
+        switch( encoding ) {
+            case FULL:
+                retval[0] = n_cities*(n_cities-1)+2;
+                retval[1] = (n_cities-1)*(n_cities-2);
+                break;
+            case RANDOMKEYS:
+                retval[0] = 0;
+                retval[1] = 0;
+                break;
+            case CITIES:
+                retval[0] = 1;
+                retval[1] = 0;
+                break;
+        }
+        return retval;
+    }
+
+    void tsp_ads::objfun_impl(fitness_vector &f, const decision_vector& x) const 
+    {
+        f[0]=0;
+        decision_vector tour;
+        decision_vector::size_type n_cities = get_n_cities();
+        double cum_p, saved_length;
+        decision_vector::size_type dumb1, dumb2;
+
+        switch( get_encoding() ) {
+            case FULL:
+            {
+                tour = full2cities(x);
+                break;
+            }
+            case RANDOMKEYS:
+            {
+                tour = randomkeys2cities(x);
+                break;
+           }
+            case CITIES:
+           {
+                tour = x;
+                break;
+           }
+        }
+        find_city_subsequence(tour, cum_p, saved_length, dumb1, dumb2);
+        f[0] = -(cum_p + (1 - m_min_value) * n_cities + saved_length / m_max_path_length);
+        return;
+    }
+
+
+    /// Computes the best subpath of an hamilonian path satisfying the max_path_length constraint
+    /**
+     * Computes the best subpath of an hamilonian path satisfying the max_path_length constraint.
+     * If the input tour does not represent an Hamiltonian path, (i.e. its an unfeasible chromosome)
+     * the algorithm behaviour is undefined
+     *
+     * @param[in]  tour the hamiltonian path encoded with a CITIES encoding (i.e. the list of cities ids)
+     * @param[out] retval_p the total cumulative value of the subpath
+     * @param[out] retval_l the total cumulative length of the subpath
+     * @param[out] retval_it_l the id of the city where the subpath starts
+     * @param[out] retval_it_r the id of the city where the subpath ends
+     * @throws value_error if the input tour length is not equal to the city number
+
+     */
+    void tsp_ads::find_best_selection(const decision_vector& tour, double& retval_p, double& retval_l, decision_vector::size_type& retval_it_l, decision_vector::size_type& retval_it_r) const
+    {
+        if (tour.size() != get_n_cities())
+        {
+            pagmo_throw(value_error, "tour dimension must be equal to the number of planets");
+        }
+
+        // We declare the necessary variable
+        decision_vector::size_type n_cities = get_n_cities();
+        decision_vector::size_type it_l = 0, it_r = 0;
+        bool cond_r = true, cond_l = true;
+        double cum_p = m_values[tour[0]];
+        double saved_length = m_max_path_length;
+
+        // We initialize the starting values
+        retval_p = cum_p;
+        retval_l = saved_length;
+        retval_it_l = it_l;
+        retval_it_r = it_r;
+
+        // Main body of the double loop
+
+        while(cond_l)
+        {
+            while(cond_r) 
+            {
+                // We increment the right "pointer" updating the value and length of the path
+                saved_length -= m_weights[tour[it_r % n_cities]][tour[(it_r + 1) % n_cities]];
+                cum_p += m_values[(it_r + 1) % n_cities];
+                it_r += 1;
+
+                // We update the various retvals only if the new subpath is valid
+                if (saved_length < 0 || (it_l % n_cities == it_r % n_cities))
+                {
+                    cond_r = false;
+                }
+                else if (cum_p > retval_p)
+                {
+                    retval_p = cum_p;
+                    retval_l = saved_length;
+                    retval_it_l = it_l % n_cities;
+                    retval_it_r = it_r % n_cities;
+                }
+                else if (cum_p == retval_p)
+                {
+                    if (saved_length > retval_l)
+                    {
+                        retval_p = cum_p;
+                        retval_l = saved_length;
+                        retval_it_l = it_l % n_cities;
+                        retval_it_r = it_r % n_cities;
+                    }
+                }
+            }
+            // We get out if all cities are included in the current path
+            if (it_l % n_cities == it_r % n_cities)
+            {
+                cond_l = false;
+            }
+            else
+            {
+                // We increment the left "pointer" updating the value and length of the path
+                saved_length += m_weights[tour[it_l % n_cities]][tour[(it_l + 1) % n_cities]];
+                cum_p -= m_values[it_l];
+                it_l += 1;
+                // We update the various retvals only if the new subpath is valid
+                if (saved_length > 0)
+                {
+                    cond_r = true;
+                    if (cum_p > retval_p)
+                    {
+                        retval_p = cum_p;
+                        retval_l = saved_length;
+                        retval_it_l = it_l % n_cities;
+                        retval_it_r = it_r % n_cities;
+                    }
+                    else if (cum_p == retval_p)
+                    {
+                        if (saved_length > retval_l)
+                        {
+                            retval_p = cum_p;
+                            retval_l = saved_length;
+                            retval_it_l = it_l % n_cities;
+                            retval_it_r = it_r % n_cities;
+                        }
+                    }
+                }
+                if (it_l == n_cities)
+                {
+                    cond_l = false;
+                }
+            }
+        }
+    }
+
+    size_t tsp_ads::compute_idx(const size_t i, const size_t j, const size_t n) const
+    {
+        pagmo_assert( i!=j && i<n && j<n );
+        return i*(n-1) + j - (j>i? 1:0);
+    }
+
+    void tsp_ads::compute_constraints_impl(constraint_vector &c, const decision_vector& x) const 
+    {
+        decision_vector::size_type n_cities = get_n_cities();
+
+        switch( get_encoding() ) 
+        {
+            case FULL:
+            {
+                // 1 - We set the equality constraints
+                for (size_t i = 0; i < n_cities; i++) {
+                    c[i] = 0;
+                    c[i+n_cities] = 0;
+                    for (size_t j = 0; j < n_cities; j++) {
+                        if(i==j) continue; // ignoring main diagonal
+                        decision_vector::size_type rows = compute_idx(i, j, n_cities);
+                        decision_vector::size_type cols = compute_idx(j, i, n_cities);
+                        c[i] += x[rows];
+                        c[i+n_cities] += x[cols];
+                    }
+                    c[i] = c[i]-1;
+                    c[i+n_cities] = c[i+n_cities]-1;
+                }
+
+                //2 - We set the inequality constraints
+                //2.1 - First we compute the uj (see http://en.wikipedia.org/wiki/Travelling_salesman_problem#Integer_linear_programming_formulation)
+                //      we start always out tour from the first city, without loosing generality
+                size_t next_city = 0,current_city = 0;
+                std::vector<int> u(n_cities);
+                for (size_t i = 0; i < n_cities; i++) {
+                    u[current_city] = i+1;
+                    for (size_t j = 0; j < n_cities; j++) 
+                    {
+                        if (current_city==j) continue;
+                        if (x[compute_idx(current_city, j, n_cities)] == 1) 
+                        {
+                            next_city = j;
+                            break;
+                        }
+                    }
+                    current_city = next_city;
+                }
+                int count=0;
+                for (size_t i = 1; i < n_cities; i++) {
+                    for (size_t j = 1; j < n_cities; j++) 
+                    {
+                        if (i==j) continue;
+                        c[2*n_cities+count] = u[i]-u[j] + (n_cities+1) * x[compute_idx(i, j, n_cities)] - n_cities;
+                        count++;
+                    }
+                }
+                break;
+            }
+            case RANDOMKEYS:
+                break;
+            case CITIES:
+            {
+                std::vector<population::size_type> range(n_cities);
+                for (std::vector<population::size_type>::size_type i=0; i<range.size(); ++i) 
+                {
+                    range[i]=i;
+                }
+                c[0] = !std::is_permutation(x.begin(),x.end(),range.begin());
+                break;
+            }
+        }
+        return;
+    }
+
+    /// Computation of the phaseless distance between two planets
+    /**
+     * @parameter[in] i index of the first orbiting object
+     * @parameter[in] j index of the second orbiting object
+     * @return the distance between m_planets[i] and m_planets[j]
+     */
+    double tsp_ads::distance(decision_vector::size_type i, decision_vector::size_type j) const
+    {
+        // TODO: Edelbaum here instead? (see paper from Gatto-Casalino)
+        using namespace std;
+        double MU = m_planets[i]->get_mu_central_body();
+        kep_toolbox::array6D elements1 =  m_planets[i]->get_elements();
+        kep_toolbox::array6D elements2 =  m_planets[j]->get_elements();
+
+        double a1 = elements1[0];
+        double i1 = elements1[2];
+        double W1 = elements1[3];
+        double e1 = elements1[1];
+
+        double a2 = elements2[0];
+        double i2 = elements2[2];
+        double W2 = elements2[3];
+        double e2 = elements2[1];
+        // radius of apocenter starting orbit (km)
+        double ra1 = a1 * (1. + e1);
+        // radius of apocenter target orbit(km)
+        double ra2 = a2 * (1. + e2)
+        // relative inclination between orbits
+        cosiREL = cos(i1) * cos(i2) + sin(i1) * sin(i2) * cos(W1) * cos(W2) + sin(i1) * sin(i2) * sin(W1) * sin(W2);
+
+        // radius of pericenter target orbit(km)
+        double rp2 = a2 * (1. - e2)
+        // radius of pericenter starting orbit (km)
+        double rp1 = a1 * (1. - e1)
+
+        if (ra1 > ra2) { // Strategy is Apocenter-Pericenter
+            Vi = sqrt(MU * (2. / ra1 - 1. / a1));
+            Vf = sqrt(MU * (2. / ra1 - 2. / (rp2 + ra1)));
+            // Change Inclination + pericenter change
+            DV1 = sqrt(Vi * Vi + Vf * Vf - 2. * Vi * Vf * cosiREL);
+            // Apocenter Change
+            DV2 = sqrt(MU) * abs(sqrt(2. / rp2 - 2. / (rp2 + ra1)) - sqrt(2. / rp2 - 2. / (rp2 + ra2)));
+        }
+        else {  // (ra1<ra2) Strategy is Pericenter-Apocenter
+            // Apocenter Raise
+            DV1 = sqrt(MU) * abs(sqrt(2. / rp1 - 2. / (rp1 + ra1)) - sqrt(2. / rp1 - 2. / (rp1 + ra2)));
+            Vi = sqrt(MU * (2. / ra2 - 2. / (rp1 + ra2)));
+            Vf = sqrt(MU * (2. / ra2 - 1. / a2));
+            // Change Inclination + apocenter change
+            DV2 = sqrt(abs(Vi * Vi + Vf * Vf - 2 * Vi * Vf * cosiREL));
+        }
+        return DV1 + DV2;
+    }
+
+    /// Getter for m_planets
+    /**
+     * @return const reference to m_planets
+     */
+    const std::vector<kep_toolbox::planet_ptr>& tsp_ads::get_planets() const
+    { 
+        return m_planets; 
+    }
+
+    /// Getter for m_max_DV
+    /**
+     * @return m_max_DV
+     */
+    const double tsp_ads::get_max_DV() const
+    { 
+        return m_max_DV; 
+    }
+
+
+    /// Getter for m_values
+    /**
+     * @return const reference to m_values
+     */
+    const std::vector<double>&  tsp_ads::get_values() const
+    { 
+        return m_values; 
+    }
+
+    /// Getter for m_times
+    /**
+     * @return const reference to m_times
+     */
+    decision_vector tsp_ads::get_times() const
+    { 
+        return m_times; 
+    }
+
+    /// Getter for m_waiting_time
+    /**
+     * @return m_waiting_time
+     */
+    const double tsp_ads::get_waiting_time() const
+    { 
+        return m_waiting_time; 
+    }
+
+    /// Returns the problem name
+    std::string tsp_ads::get_name() const
+    {
+        return "Asteroids / Debris Selection TSP (TSP-ADS)";
+    }
+
+    /// Extra human readable info for the problem.
+    /**
+     * @return a std::string containing a list of vertices and edges
+     */
+    std::string tsp_ads::human_readable_extra() const 
+    {
+        std::ostringstream oss;
+        oss << "\n\tNumber of cities: " << get_n_cities() << '\n';
+        oss << "\tEncoding: ";
+        switch( get_encoding()  ) {
+            case FULL:
+                oss << "FULL" << '\n';
+                break;
+            case RANDOMKEYS:
+                oss << "RANDOMKEYS" << '\n';
+                break;
+            case CITIES:
+                oss << "CITIES" << '\n';
+                break;
+        }
+        oss << "\tCities Values: " << m_values << std::endl;
+        oss << "\tMax path length: " << m_max_path_length << '\n';
+        oss << "\tWeight Matrix: \n";
+        for (decision_vector::size_type i=0; i<get_n_cities() ; ++i)
+        {
+            oss << "\t\t" << m_weights.at(i) << '\n';
+            if (i>5)
+            {
+                oss << "\t\t..." << '\n';
+                break;
+            }
+        }
+        return oss.str();
+    }
+
+    
+}} //namespaces
+
+BOOST_CLASS_EXPORT_IMPLEMENT(pagmo::problem::tsp_ads)
---
+++ src/problem/tsp_ads.h
@@ -0,0 +1,135 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2014 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_PROBLEM_TSP_ADS_H
+#define PAGMO_PROBLEM_TSP_ADS_H
+
+#include <vector>
+#include <string>
+
+#include "../types.h"
+#include "./base_tsp.h"
+#include "../serialization.h"
+#include "../keplerian_toolbox/planet.h"
+
+namespace pagmo { namespace problem {
+
+/// The TSP - Asteroids-Debris Selection Problem
+/**
+ * This is a class representing a new variant of the TSP which refines the pagmo::problem::tsp_cs problem adding time.
+ * While in the City Selection Problem (pagmo::problem::tsp_cs)
+ * the objective function computes the cost of transferring between cities using a static weight matrix, here it is
+ * computed using a Lambert's orbital transfer model where the transfer times are stored in the m_times data members. 
+ * The distance virtual method is instead providing  a time independent approximation to such a distance.
+ * 
+ * The idea is that this problem can be used in a bi-level optimization where m_times is optimized in the outer layer 
+ *
+ * The information about transfer times is contained in the data member m_times which contains \f$ [t_0,T_1,T_2,T_3,....T_{N-1}] \f$
+ * where \f$ t_0 \f$ is the mission starting epoch (MJD2000) and \f$ T_i \f$ is the transfer time (in days) from \f$ x[i-1]\f$ to \f$ x[i]\f$.
+ * 
+ * The possibility to use a waiting time is also provided via the data member m_waiting_time (in days)
+ *
+ * @author Dario Izzo (dario.izzo@gmail.com)
+ */
+
+class __PAGMO_VISIBLE tsp_ads: public base_tsp
+{
+    public:
+
+        /// Constructor
+        tsp_ads(
+            const std::vector<kep_toolbox::planet_ptr>& planets, 
+            const std::vector<double>& values,
+            const double max_DV, 
+            const std::vector<double>&  times, 
+            const double waiting_time = 0., 
+            const base_tsp::encoding_type & encoding = CITIES
+        );
+
+        /// Copy constructor for polymorphic objects
+        base_ptr clone() const;
+
+        /// Given an hamiltonian path finds the best subtours (accounting for the Lambert's problem)
+        void find_best_selection(const decision_vector &, double &, double &, decision_vector::size_type &, decision_vector::size_type &) const;
+
+        /** @name Getters*/
+        //@{
+        const std::vector<kep_toolbox::planet_ptr>& get_planets() const;
+        const std::vector<double>& get_values() const;
+        double get_max_path_length() const;
+        const decision_vector& get_times() const;
+        double get_waiting_time() const;
+        //@}
+
+        /** @name Setters*/
+        //@{
+        void set_times(const decision_vector &);
+        //@}
+
+        /** @name Implementation of virtual methods*/
+        //@{
+        std::string get_name() const;
+        std::string human_readable_extra() const;
+        double distance(decision_vector::size_type, decision_vector::size_type) const;
+        //@}
+
+    private:
+        static boost::array<int, 2> compute_dimensions(decision_vector::size_type n_cities, base_tsp::encoding_type);
+        void check_weights(const std::vector<std::vector<double> >&) const;
+        size_t compute_idx(const size_t i, const size_t j, const size_t n) const;
+
+        void objfun_impl(fitness_vector&, const decision_vector&) const;
+        void compute_constraints_impl(constraint_vector&, const decision_vector&) const;
+
+        friend class boost::serialization::access;
+        template <class Archive>
+        void serialize(Archive &ar, const unsigned int)
+        {
+            ar & boost::serialization::base_object<base_tsp>(*this);
+            ar & const_cast<std::vector<kep_toolbox::planet_ptr> &>m_planets;
+            ar & m_values;
+            ar & const_cast<double &>(m_max_DV);
+            ar & m_times;
+            ar & m_waiting;
+            ar & m_min_value;
+        }
+
+    private:
+        const std::vector<kep_toolbox::planet_ptr> m_planets;
+        std::vector<double> m_values;
+        const double m_max_DV;
+        decision_vector m_times;
+        double m_waiting_time;
+
+        // this data member is set in the constructor as the minimum in m_values and is used in the objecive function
+        double m_min_value;
+
+
+};
+
+}}  //namespaces
+
+BOOST_CLASS_EXPORT_KEY(pagmo::problem::tsp_ads)
+
+#endif  //PAGMO_PROBLEM_TSP_ADS_H
