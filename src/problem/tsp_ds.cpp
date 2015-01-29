8467416440908df81175da20a575df44df00b436
Dario Izzo <dario.izzo@gmail.com>
2015-01-29 22:57:10 +0100

    Added a header missing that was failing clang compilatio

--- src/problem/tsp_ds.cpp
+++ src/problem/tsp_ds.cpp
@@ -22,6 +22,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
  *****************************************************************************/
 
+#include <numeric>
+
 #include "tsp_ds.h"
 #include "../exceptions.h"
 #include "../population.h"
