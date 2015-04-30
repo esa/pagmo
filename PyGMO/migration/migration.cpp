4e577a6006d8382dc694e8ec6ab1718f33470f0d
Francesco Biscani <bluescarni@gmail.com>
2015-04-30 14:38:54 +0200

    First attempt at fixing the includes for MinGW64.

--- PyGMO/migration/migration.cpp
+++ PyGMO/migration/migration.cpp
@@ -22,6 +22,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
  *****************************************************************************/
 
+#include <cmath>
 #include <boost/python/class.hpp>
 #include <boost/python/enum.hpp>
 #include <boost/python/module.hpp>
